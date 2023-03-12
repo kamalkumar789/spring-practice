910c469557afd7276479759a60292abc43d03926
kamal <kamalkumargit@gmail.com>
2023-03-13 01:16:04 +0500

    changed controller

--- src/main/java/com/example/cloudserver/Controllerr.java
+++ src/main/java/com/example/cloudserver/Controllerr.java
@@ -2,6 +2,7 @@ package com.example.cloudserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class Controllerr {
 
     @GetMapping("/")
     private String getUsernameAndPassword(){
+        return configuration.getUsername();
+    }
+
+    @PostMapping("/check")
+    private String getUserNameAndPassword(){
         return configuration.getUsername();
     }
 }
